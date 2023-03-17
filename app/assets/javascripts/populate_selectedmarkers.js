4c540eda9a30e1e03eb774f36da079fd8e960e58
kary <39820@ufp.edu.pt>
2023-03-17 14:37:28 +0000

    1

--- app/assets/javascripts/populate_selectedmarkers.js
+++ app/assets/javascripts/populate_selectedmarkers.js
@@ -1,13 +1,15 @@
 function populateSelectedMarkersTable(selectedMarkers) {
-    var tableBody = document.getElementById("selected-markers-table").getElementsByTagName("tbody")[0];
-    tableBody.innerHTML = "";
-    for (var i = 0; i < selectedMarkers.length; i++) {
-      var row = tableBody.insertRow(i);
-      var nameCell = row.insertCell(0);
-      var priceCell = row.insertCell(1);
-      var locationCell = row.insertCell(2);
-      nameCell.innerHTML = selectedMarkers[i].Title;
-      priceCell.innerHTML = selectedMarkers[i].Price;
-      locationCell.innerHTML = selectedMarkers[i].Localizacao;
-    }
+  var tableBody = document.getElementById("selected-markers-table").getElementsByTagName("tbody")[0];
+  for (var i = 0; i < selectedMarkers.length; i++) {
+    var row = tableBody.insertRow();
+    var nameCell = row.insertCell(0);
+    var priceCell = row.insertCell(1);
+    var locationCell = row.insertCell(2);
+    var link = document.createElement("a"); 
+    link.href = selectedMarkers[i].Url; 
+    link.innerHTML = selectedMarkers[i].Title; 
+    nameCell.appendChild(link); 
+    priceCell.innerHTML = selectedMarkers[i].Price;
+    locationCell.innerHTML = selectedMarkers[i].Localizacao;
+  }
 }
