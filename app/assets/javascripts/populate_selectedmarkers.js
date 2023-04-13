461ac58a838e2e1f20fba975cd948f99649dcd4e
kary <39820@ufp.edu.pt>
2023-04-13 22:54:43 +0100

    price-range map

--- app/assets/javascripts/populate_selectedmarkers.js
+++ app/assets/javascripts/populate_selectedmarkers.js
@@ -1,25 +1,41 @@
-function populateSelectedMarkersTable(selectedHouses, numColumns, sortOrder) {
+function populateSelectedMarkersTable(selectedHouses, numColumns, sortOrder, minPrice, maxPrice, showNullPrices) {
+
+console.log(minPrice, maxPrice, showNullPrices, sortOrder);
+
+
+if ((minPrice !== null && maxPrice !== null) || showNullPrices) {
+  selectedHouses = selectedHouses.filter(function (house) {
+    const price = parseFloat(house.Price);
+    console.log(price);
+    if (showNullPrices) { 
+      return (isNaN(price) || price >= minPrice && price <= maxPrice);
+    } else {
+      return (price >= minPrice && price <= maxPrice);
+    }
+  });
+}
+
+  
 
   if (sortOrder) {
     switch (sortOrder) {
       case "price-low-to-high":
-        selectedHouses.sort(function(a, b) {
-          console.log(a.Price - b.Price)
+        selectedHouses.sort(function (a, b) {
           return a.Price - b.Price;
         });
         break;
       case "price-high-to-low":
-        selectedHouses.sort(function(a, b) {
+        selectedHouses.sort(function (a, b) {
           return b.Price - a.Price;
         });
         break;
       case "area-small-to-large":
-        selectedHouses.sort(function(a, b) {
+        selectedHouses.sort(function (a, b) {
           return a.Ano_de_Construcao - b.Ano_de_Construcao;
         });
         break;
       case "area-large-to-small":
-        selectedHouses.sort(function(a, b) {
+        selectedHouses.sort(function (a, b) {
           return b.Ano_de_Construcao - a.Ano_de_Construcao;
         });
         break;
@@ -80,13 +96,13 @@ function populateSelectedMarkersTable(selectedHouses, numColumns, sortOrder) {
     }
     cell = row.insertCell(houseIndex);
     html = template.replace('{{imageUrl}}', selectedHouses[i].ImageUrl || 'img/house-default.jpg')
-                      .replace('{{location}}', selectedHouses[i].Localizacao)
-                      .replace('{{status}}', selectedHouses[i].Situacao)
-                      .replace('{{price}}', selectedHouses[i].Price|| '')
-                      .replace('{{id}}', selectedHouses[i].Id)
-                      .replace('{{area}}', selectedHouses[i].Area_Util || '')
-                      .replace('{{bedrooms}}', selectedHouses[i].Total_quartos || '')
-                      .replace('{{condition}}', selectedHouses[i].Condicao || '');
+      .replace('{{location}}', selectedHouses[i].Localizacao)
+      .replace('{{status}}', selectedHouses[i].Situacao)
+      .replace('{{price}}', selectedHouses[i].Price || '')
+      .replace('{{id}}', selectedHouses[i].Id)
+      .replace('{{area}}', selectedHouses[i].Area_Util || '')
+      .replace('{{bedrooms}}', selectedHouses[i].Total_quartos || '')
+      .replace('{{condition}}', selectedHouses[i].Condicao || '');
     cell.innerHTML = html;
   }
-  }
+}
